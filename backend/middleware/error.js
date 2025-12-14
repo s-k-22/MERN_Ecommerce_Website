c51b3c67a98026bbed48fe609bcb21a12af95722
s-k-22 <swar.khatale@gmail.com>
2025-12-14 11:21:10 +0530

    Error Handling ->  custom error class, err middleware, promise rejection, exception handling, mongodb errors

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,6 +1,13 @@
+import HandleError from "../utils/handleError.js";
+
 export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
+
+  if (err.name === "CastError") {
+    const message = `Invalid  resource ${err.path}`;
+    err = new HandleError(message, 404);
+  }
 
   res.status(err.statusCode).json({
     success: false,
--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,23 @@ dotenv.config({ path: "backend/config/config.env" });
 
 connectMongoDatabase();
 
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Server is shutting down, due to uncaught exception`);
+  process.exit(1);
+});
+
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server is running on ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`server is shutting down, due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
