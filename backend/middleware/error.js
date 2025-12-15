73e553bce31cff4c24c37d686092962c84c78a18
s-k-22 <swar.khatale@gmail.com>
2025-12-15 14:48:34 +0530

    mongodb duplicate key error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,8 +4,17 @@ export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
+  //for cast error -> 99% it's about _id -> invalid value is provided for a field that requires a specific data type
   if (err.name === "CastError") {
     const message = `Invalid  resource ${err.path}`;
+    err = new HandleError(message, 404);
+  }
+
+  //for unique fields in db
+  if (err.code === 11000) {
+    const message = `This ${Object.keys(
+      err.keyValue
+    )} already registered. Please Login to continue...`;
     err = new HandleError(message, 404);
   }
 
