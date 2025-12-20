1436f9983824df16225998f73416581f04f2e38d
s-k-22 <swar.khatale@gmail.com>
2025-12-20 15:16:23 +0530

    nothing imp -> updated comments

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -5,7 +5,6 @@ import User from "../models/userModel.js";
 
 export const verifyUserAuth = wrapAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
-  console.log(token);
 
   if (!token) {
     return next(
--- backend/utils/handleError.js
+++ backend/utils/handleError.js
@@ -1,8 +1,8 @@
 class HandleError extends Error {
   constructor(message, statusCode) {
-    super(message);
+    super(message); //Error class has msg but not statuscode
     this.statusCode = statusCode;
-    Error.captureStackTrace(this,this.constructor)
+    Error.captureStackTrace(this, this.constructor); //without Error.captureStackTrace -> lot of messages on terminal when error occurs
   }
 }
 
